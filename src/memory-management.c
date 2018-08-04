ca3638166a5a477ca00b1eb3994b6d3e93588173
gibber9809 <gibber9809@users.noreply.github.com>
2018-08-04 18:22:21 -0400

    Implement simple dynamic object allocation

--- src/memory-management.c
+++ src/memory-management.c
@@ -25,5 +25,40 @@ void* class_alloc(size_t size) {
 
 void* object_alloc(size_t size) {
     // Dummy implementation
-    return (void*)heap.dyn_object_start;
+    Object* object;
+    size_t total_size = size + sizeof(Object);
+
+    if (heap.first_object == NULL) {
+        heap.first_object = (Object*) heap.dyn_object_start;
+        object = heap.last_object = heap.first_object;
+
+        if (heap.heap_end - heap.dyn_object_start < total_size) {
+            // Extend the heap to be long enough
+            if (sbrk(total_size - (heap.heap_end - heap.dyn_object_start)) == (void*)-1)
+                return (void*) -1;
+            else
+                heap.heap_end = (long) object + total_size;
+        }
+
+        object->next = object->prev = NULL;
+        object->size = size;
+        
+        return ((void*) object) + sizeof(Object);
+    } 
+    
+    object = (Object*) (((void*)heap.last_object) + heap.last_object->size + sizeof(Object));
+    object = (Object*) (ALIGN((long)object));
+    if (heap.heap_end - (long) object < total_size) {
+        if (sbrk(total_size - (long)object) == (void*)-1)
+            return (void*) -1;
+        else 
+            heap.heap_end = (long) object + total_size;
+    }
+    
+    heap.last_object->next = object;
+    object->prev = heap.last_object;
+    object->next = NULL;
+    heap.last_object = object;
+    
+    return ((void*) object) + sizeof(Object);
 }

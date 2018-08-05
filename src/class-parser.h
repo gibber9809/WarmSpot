d70cd08fe8d983795c5c591b0edfd1b743f080d2
gibber9809 <gibber9809@users.noreply.github.com>
2018-08-05 00:12:57 -0400

    Index constant pool on class load

--- src/class-parser.h
+++ src/class-parser.h
@@ -3,6 +3,64 @@
 
 #include <stdint.h>
 
+#define MAGIC 0xCAFEBABE
+
+#define CONSTANT_Utf8               1
+#define CONSTANT_Integer            3
+#define CONSTANT_Float              4
+#define CONSTANT_Long               5
+#define CONSTANT_Double             6
+#define CONSTANT_Class              7
+#define CONSTANT_String             8
+#define CONSTANT_Fieldref           9
+#define CONSTANT_Methodref          10
+#define CONSTANT_InterfaceMethodref 11
+#define CONSTANT_NameAndType        12
+#define CONSTANT_MethodHandle       15
+#define CONSTANT_MethodType         16
+#define CONSTANT_InvokeDynamic      18
+
+/* 
+ * Common Access and Property Flags
+ */
+
+#define ACC_PUBLIC    0x0001
+#define ACC_PRIVATE   0x0002
+#define ACC_PROTECTED 0x0004
+#define ACC_STATIC    0x0008
+#define ACC_FINAL     0x0010
+#define ACC_SYNTHETIC 0x1000
+
+/*
+ * Class Access and Property Flags
+ */
+#define ACC_SUPER      0x0020
+#define ACC_INTERFACE  0x0200
+#define ACC_ABSTRACT   0x0400
+#define ACC_ANNOTATION 0x2000
+#define ACC_ENUM       0x4000
+
+/*
+ * Field Access and Property Flags
+ */
+
+#define ACC_VOLATILE  0x0040
+#define ACC_TRANSIENT 0x0080    
+#define ACC_ENUM      0x4000
+
+/*
+ * Method Access and Property Flags
+ * 
+ * Shares public, private, protected, static final, 
+ * with field flags
+ */
+#define ACC_SYNCHRONIZED 0x0020
+#define ACC_BRIDGE       0x0040
+#define ACC_VARARGS      0x0080
+#define ACC_NATIVE       0x0100
+#define ACC_ABSTRACT     0x0400
+#define ACC_STRICT       0x0800
+
 typedef struct {
     uint16_t minor_version;
     uint16_t major_version;
@@ -21,6 +79,6 @@ typedef struct {
     void** attribute_index;
 } Class;
 
-void initialize_class_from_file(const char* file_name);
+int initialize_class_from_file(const char* file_name);
 
 #endif
--- src/endian-utils.c
+++ src/endian-utils.c
@@ -2,12 +2,52 @@
 #include <byteswap.h>
 #include <stdint.h>
 
+static void _slow_get2byte(char* dst, char* src);
+static void _slow_get4byte(char* dst, char* src);
+static void _slow_get8byte(char* dst, char* src);
+
+
 void get2byte(uint16_t* dst, uint16_t* src) {
-    *dst = bswap_16(*src);
+    if ( ((long)dst) & 0x01 == 0 && ((long)src) & 0x01 == 0) 
+        *dst = bswap_16(*src);
+    else
+        _slow_get2byte((char*) dst, (char*) src);
 }
 void get4byte(uint32_t* dst, uint32_t* src) {
-    *dst = bswap_32(*src);
+    if ( ((long)dst) & 0x03 == 0 && ((long)src)& 0x03 == 0)
+        *dst = bswap_32(*src);
+    else
+        _slow_get4byte((char*) dst, (char*) src);
 }
 void get8byte(uint64_t* dst, uint64_t* src) {
-    *dst = bswap_64(*src);
+    if( ((long)dst) & 0x07 == 0 && ((long)src) & 0x07 == 0)
+        *dst = bswap_64(*src);
+    else
+        _slow_get8byte((char*) dst, (char*) src);
+}
+
+
+/*
+ * Byte by byte transfer because either dst or src
+ * is not aligned.
+ */
+static void _slow_get2byte(char* dst, char* src) {
+    dst[0] = src[1];
+    dst[1] = src[0];
+}
+static void _slow_get4byte(char* dst, char* src) {
+    dst[0] = src[3];
+    dst[1] = src[2];
+    dst[2] = src[1];
+    dst[3] = src[0];
+}
+static void _slow_get8byte(char* dst, char* src) {
+    dst[0] = src[7];
+    dst[1] = src[6];
+    dst[2] = src[5];
+    dst[3] = src[4];
+    dst[4] = src[3];
+    dst[5] = src[2];
+    dst[6] = src[1];
+    dst[7] = src[0];
 }

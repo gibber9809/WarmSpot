fcca3b0b05a0b9fb6f1a6dee03d42597c071d90c
gibber9809 <gibber9809@users.noreply.github.com>
2018-08-23 23:34:12 -0400

    Add jtypes.h to represent java types

---
+++ src/jtypes.c
@@ -0,0 +1,30 @@
+#include "jtypes.h"
+
+#include <stdlib.h>
+
+size_t jsizeof(char* jtype) {
+    switch(*jtype) {
+        case 'B':
+            return sizeof(jbyte);
+        case 'C':
+            return sizeof(jchar);
+        case 'D':
+            return sizeof(jdouble);
+        case 'F':
+            return sizeof(jfloat);
+        case 'I':
+            return sizeof(jint);
+        case 'J':
+            return sizeof(jlong);
+        case 'L':
+            return sizeof(jref);
+        case 'S':
+            return sizeof(jshort);
+        case 'Z':
+            return sizeof(jbool);
+        case '[':
+            return sizeof(jref);
+        default:
+            return 0;
+    }
+}

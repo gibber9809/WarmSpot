930a11dc462cd9ac10e9962121f7c79d82f2d8ea
gibber9809 <gibber9809@users.noreply.github.com>
2018-08-26 02:25:42 -0400

    Implement new_stackframe function

--- src/cpu.c
+++ src/cpu.c
@@ -43,7 +43,7 @@ Cpu* init_first_cpu(Class* class, int argc, const char** argv) {
     push_opstack_ref(mock_opstack, &mock_opstack_top, (char*) mock_opstack_data, NULL, JARRAY);
 
     // call new_stackframe with void main(String[]) on initial class
-    //frame = new_stackframe(class, method_info.attributes, NULL);
+    frame = new_stackframe(class, &method_info, NULL);
 
     // call push_method_arguments with mocked stackframe and new stackframe
     //push_method_arguments(frame, mock_opstack, mock_opstack, (char*) mock_opstack_data, &mock_opstack_top)
@@ -51,8 +51,32 @@ Cpu* init_first_cpu(Class* class, int argc, const char** argv) {
     return NULL;
 }
 
-StackFrame* new_stackframe(Class* class, uint8_t* code_attribute, StackFrame* prev_frame) {
-    return NULL;
+StackFrame* new_stackframe(Class* class, MethodInfo* method, StackFrame* prev_frame) {
+    size_t alloc_size;
+    StackFrame* rframe;
+    Code code;
+
+    get_code(method, &code);
+
+    // This can be done more cleverly to reduce the size of the allocation
+    alloc_size = ALIGN(sizeof(StackFrame)) + (sizeof(StackVariable) + sizeof(jlong)) * (code.max_stack + code.max_locals);
+
+    rframe = (StackFrame*) object_alloc(alloc_size);
+
+    rframe->code = code.code;
+    rframe->exception_table = code.exc_table_len > 0 ? code.exception_table : NULL;
+    rframe->exc_table_len = code.exc_table_len;
+    rframe->max_locals = code.max_locals;
+    rframe->opstack_top = 0;
+    rframe->local_vars_base =  ((uint8_t*)rframe) + ALIGN(sizeof(StackFrame));
+    rframe->opstack_base = rframe->local_vars_base + code.max_locals * sizeof(jlong);
+    rframe->local_vars = (StackVariable*) (rframe->opstack_base + code.max_stack * sizeof(jlong));
+    rframe->opstack =  rframe->local_vars + code.max_locals;
+    rframe->prev_frame = prev_frame;
+    rframe->pc = 0;
+    rframe->class = class;
+
+    return rframe;
 }
 
 void push_method_arguments(StackFrame* new_stackframe, StackVariable* prev_opstack, char* prev_opstack_data, uint16_t* prev_opstack_top, uint8_t* method_descriptor) {
@@ -61,7 +85,7 @@ void push_method_arguments(StackFrame* new_stackframe, StackVariable* prev_opsta
 
 void push_opstack_ref(StackVariable* opstack, uint16_t* opstack_top, char* opstack_data, jref* data, uint16_t reftype) {
     uint16_t top = *opstack_top, prev = *opstack_top - 1;
-    prev = opstack[prev].type == 0 ? prev - 1 : prev;
+    prev = (*opstack_top > 0 && opstack[prev].type == 0) ? prev - 1 : prev;
 
     if (top == 0) {
         opstack[0].offset = 0;
@@ -78,8 +102,8 @@ void push_opstack_ref(StackVariable* opstack, uint16_t* opstack_top, char* opsta
 
 void _push_opstack(StackVariable* opstack, uint16_t* opstack_top, char* opstack_data, char* data, uint16_t type, size_t nbytes, uint8_t nindeces) {
     uint16_t top = *opstack_top, prev = *opstack_top - 1;
-    prev = opstack[prev].type == 0 ? prev - 1 : prev;
-
+    prev = (*opstack_top > 0 && opstack[prev].type == 0) ? prev - 1 : prev;
+    
     if (top == 0) {
         opstack[0].offset = 0;
         opstack[0].type = type;
